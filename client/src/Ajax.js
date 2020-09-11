c7b21aad4de024a6eca32134c66c7517e44e1cc3
Curt Morgan <curtmorgan3@gmail.com>
2020-09-11 19:34:40 -0400

    Updated Settings and SignUp components

--- client/src/Ajax.js
+++ client/src/Ajax.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://localhost:3001';
 
 export default class Ajax{
 
-	//Users
+	// Users
 	static async createUser(payload){
 		try {
 			let response = await axios.post(`${BASE_URL}/users`, payload);
@@ -44,19 +44,6 @@ export default class Ajax{
 		}
 	}
 
-	static async chargeStripe(payload){
-		try {
-			let response = await axios.post(`${BASE_URL}/charge`, payload, {headers:
-				{
-					"Content-Type": "text/plain"
-				}
-			});
-			return(response.data);
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
 	static async updateUser(payload){
 		try {
 			let token = localStorage.getItem('handy_helper_token');
@@ -71,85 +58,19 @@ export default class Ajax{
 		}
 	}
 
-	static async addFavRecipe(payload){
+	static async deleteUser(){
 		try {
-			let token = localStorage.getItem('snacks_from_scratch_user_token');
-			let response = await axios.put(`${BASE_URL}/users/add-to-favorites`, payload,
-				{headers: {
+			let token = localStorage.getItem('handy_helper_token');
+			let response = await axios.delete(`${BASE_URL}/users/`, 
+				{ headers: {
 					'Authorization': `Bearer ${token}`
 				}
-			})
+			});
+			
 			return(response.data);
 		} catch (e) {
 			console.error(e);
 		}
 	}
 
-	static async removeFavRecipe(payload){
-		try {
-			let token = localStorage.getItem('snacks_from_scratch_user_token');
-			let response = await axios.put(`${BASE_URL}/users/remove-from-favorites`, payload,
-				{headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			})
-			return(response.data);
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
-	// Recipes
-	static async getAllRecipes(token){
-		try {
-			let response = await axios.get(`${BASE_URL}/recipes`, {
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			})
-			return response.data;
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
-	static async getRecipeByID(ID, token){
-		try {
-			let response = await axios.get(`${BASE_URL}/recipes/${ID}`, {
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			})
-			return response.data;
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
-	// Ingredients
-	static async getAllIngredients(token){
-		try {
-			let response = await axios.get(`${BASE_URL}/ingredients`, {
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			})
-			return response.data;
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
-	// Email
-	static async sendEmail(user){
-		try {
-			let data = {
-				name: user.name,
-				email: user.email
-			}
-			await axios.post(`${BASE_URL}/email`, data);
-		} catch (e) {
-			console.error(e);
-		}
-	}
 }
