0abc75b1ee704eb5e483bceac9c6627cb72e2c76
Marlene Hasslinger <hasslingermarlene@gmail.com>
2020-09-13 19:12:48 -0400

    set up basic profile management

--- client/src/components/ViewProfile/index.js
+++ client/src/components/ViewProfile/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = {
   profileContainer: {
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center',
-    width: '80vw',
+    width: '80%',
     height: '100%',
+  },
+  infoContainer: {
+    width: '100%',
+    backgroundColor: '#E9ECEF',
+    borderRadius: '15px',
+    padding: '2%',
+    marginBottom: '10px',
+  },
+  button: {
+    textTransform: 'none',
+    width: '45%',
+    height: '50px',
+    backgroundColor: '#0069D9',
+    color: '#ffffff',
+    marginTop: '15px',
   },
 }
 
@@ -27,18 +42,53 @@ const ViewProfile = () => {
     )
   }
 
+  const handleChange = (e) => {
+    // TODO
+  }
+
   return (
     <div className={classes.profileContainer}>
       <h1>My Profile</h1>
+      <div className={classes.infoContainer}>
+        <h2>Basic Information</h2>
+        <form>
+          <label htmlFor='name'>Name</label><br />
+          <input type='text' name='name' id='name' value={currentUser.firstName + ' ' + currentUser.lastName} onChange={handleChange} /><br />
+          <label htmlFor='name'>Location</label><br />
+          <input type='text' name='location' id='location' value={currentUser.location} onChange={handleChange} /><br />
+          <label htmlFor='name'>Phone Number</label><br />
+          <input type='text' name='phone' id='phone' value={currentUser.phone} onChange={handleChange} /><br />
+          {currentUser.isHelper ?
+            <div>
+              <label htmlFor='name'>Skill</label><br />
+              <input type='text' name='skill' id='skill' value={currentUser.skill} onChange={handleChange} /><br />
+            </div> : null
+          }
+        </form>
+      </div>
 
-      <h3>{currentUser.firstName} {currentUser.lastName}</h3>
-      <span>{currentUser.location}</span>
+      <div className={classes.infoContainer}>
+        <h2>Account Management</h2>
+        <h3>Preferences</h3>
+        <h3>Change Password</h3>
+        <form>
+          <label htmlFor='name'>Old Password</label><br />
+          <input type='text' name='oldPassword' id='oldPassword' value='' onChange={handleChange} /><br />
+          <label htmlFor='name'>New Password</label><br />
+          <input type='text' name='newPassword' id='newPassword' value='' onChange={handleChange} /><br />
+        </form>
+        <div>
+          <Button className={classes.button}>Reset Password</Button>
+        </div>
 
-      <h4>Skill</h4>
-      <p>{currentUser.skill}</p>
+        <h3>Transactions</h3>
+        <h3>Deactivate Account</h3>
+      </div>
 
-      <h4>Experience</h4>
-      <p>{currentUser.experience}</p>
+      <div className={classes.infoContainer}>
+        <h2>Customer Support</h2>
+      </div>
+
     </div>
   )
 }
